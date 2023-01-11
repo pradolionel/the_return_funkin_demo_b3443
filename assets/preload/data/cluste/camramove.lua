da6978f95efabcaef4027786134873a16d65762d
pradolionel <99144412+pradolionel@users.noreply.github.com>
2023-01-11 12:43:11 -0500

    Add files via upload

---
+++ assets/preload/data/cluste/camramove.lua
@@ -0,0 +1,118 @@
+
+local r = 0;
+local i =0
+local shot = false;
+local agent = 1
+local health = 0;
+local xx = 1950;
+local yy = 410;
+local xx2 = 2200;
+local yy2 = 410;
+local ofs = 30;
+local followchars = true;
+local del = 0;
+local del2 = 0;
+
+function onCreatePost()
+
+    makeLuaText("message", "vs whitty - cluste", 500, 30, 25)
+    setTextAlignment("message", "left")
+    addLuaText("message")
+
+
+    makeLuaText("engineText", "Artsy - the return funkin demo", 500, 30, 20)
+    setTextAlignment("engineText", "left")
+    addLuaText("engineText")
+
+
+    if getPropertyFromClass('ClientPrefs', 'downScroll') == false then
+        setProperty('message.y', 680)
+        setProperty('engineText.y', 660)
+    end
+end
+
+
+
+function onCreate()
+
+    makeLuaSprite('bartop','',0,-30)
+    makeGraphic('bartop',1280,80,'000000')
+    addLuaSprite('bartop',false)
+    setObjectCamera('bartop','hud')
+    setScrollFactor('bartop',0,0)
+
+    makeLuaSprite('barbot','',0,670)
+    makeGraphic('barbot',1280,80,'000000')
+    addLuaSprite('barbot',false)
+    setScrollFactor('barbot',0,0)
+    setObjectCamera('barbot','hud')
+end
+
+function onUpdate(elapsed)
+
+	if del > 0 then
+		del = del - 1
+	end
+	if del2 > 0 then
+		del2 = del2 - 1
+	end
+     if followchars == true then
+        if mustHitSection == false then
+            setProperty('defaultCamZoom',1.00)
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singLEFT-alt' then
+                triggerEvent('Camera Follow Pos',xx-ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singRIGHT-alt' then
+                triggerEvent('Camera Follow Pos',xx+ofs,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singUP-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy-ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'singDOWN-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy+ofs)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+            if getProperty('dad.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx,yy)
+            end
+        else
+      
+            setProperty('defaultCamZoom',1.00)
+            if getProperty('boyfriend.animation.curAnim.name') == 'singLEFT' then
+                triggerEvent('Camera Follow Pos',xx2-ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singRIGHT' then
+                triggerEvent('Camera Follow Pos',xx2+ofs,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singUP' then
+                triggerEvent('Camera Follow Pos',xx2,yy2-ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'singDOWN' then
+                triggerEvent('Camera Follow Pos',xx2,yy2+ofs)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle-alt' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+            if getProperty('boyfriend.animation.curAnim.name') == 'idle' then
+                triggerEvent('Camera Follow Pos',xx2,yy2)
+            end
+        end
+    else
+        triggerEvent('Camera Follow Pos','','')
+    end
+    
+end
