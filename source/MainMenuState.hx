f77d3104ee233cd410d0dad39edeb12caebf6e9e
pradolionel <99144412+pradolionel@users.noreply.github.com>
2023-01-12 16:44:31 -0500

    Add files via upload

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -316,9 +316,9 @@ class MainMenuState extends MusicBeatState
 			PlayState.SONG = Song.loadFromJson(PlayState.storyPlaylist[0].toLowerCase() + diffic, PlayState.storyPlaylist[0].toLowerCase());
 			PlayState.campaignScore = 0;
 			PlayState.campaignMisses = 0;
-			new FlxTimer().start(1, function(tmr:FlxTimer)
-			{
+			
 				LoadingState.loadAndSwitchState(new PlayState(), true);
+				
 				FreeplayState.destroyFreeplayVocals();
 									case 'freeplay':
 										MusicBeatState.switchState(new FreeplayState());
